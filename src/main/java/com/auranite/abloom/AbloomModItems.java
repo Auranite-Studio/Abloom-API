bf708e30280dccf7a73cc885138a05a544269cc6
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2026-06-02 03:48:57 +0300

    Fix typo

--- src/main/java/com/auranite/abloom/AbloomModItems.java
+++ src/main/java/com/auranite/abloom/AbloomModItems.java
@@ -40,7 +40,7 @@ public class AbloomModItems {
         QUANTUM_STICK  = register("quantum_stick", properties -> new Item(properties.stacksTo(1).rarity(Rarity.RARE)));
         ETHER_STICK  = register("ether_stick", properties -> new Item(properties.stacksTo(1).rarity(Rarity.RARE)));
         LIGHT_STICK  = register("light_stick", properties -> new Item(properties.stacksTo(1).rarity(Rarity.RARE)));
-        SHADOW_STICK  = register("light_stick", properties -> new Item(properties.stacksTo(1).rarity(Rarity.RARE)));
+        SHADOW_STICK  = register("shadow_stick", properties -> new Item(properties.stacksTo(1).rarity(Rarity.RARE)));
     }
 
     private static <I extends Item> DeferredItem<I> register(String name, Function<Item.Properties, ? extends I> supplier) {
