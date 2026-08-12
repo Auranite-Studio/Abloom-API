91b2a503fb3fd9de53fde7f439beee2b7bcb47dc
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2026-08-12 04:00:35 +0300

    Fix porting

--- src/main/java/com/auranite/abloom/effect/PrismEffect.java
+++ src/main/java/com/auranite/abloom/effect/PrismEffect.java
@@ -1,5 +1,6 @@
 package com.auranite.abloom.effect;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -16,8 +17,8 @@ public class PrismEffect extends MobEffect {
     }
 
     @Override
-    public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-        if (entity.level().isClientSide) {
+    public boolean applyEffectTick(ServerLevel serverLevel, LivingEntity entity, int amplifier) {
+        if (entity.level().isClientSide()) {
             return true;
         }
         // Prism effect doesn't deal damage on its own - it only converts incoming prism damage
