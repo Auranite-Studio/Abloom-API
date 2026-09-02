2cba2240007075d1d9ec2a953bb5107749f760ff
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2026-09-02 03:26:11 +0300

    Some improvements

--- src/main/java/com/auranite/abloom/effect/FluorescenceEffect.java
+++ src/main/java/com/auranite/abloom/effect/FluorescenceEffect.java
@@ -1,13 +1,29 @@
 package com.auranite.abloom.effect;
 
+import com.auranite.abloom.init.AbloomModAttachments;
+import com.auranite.abloom.util.ElementType;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
-import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.LivingEntity;
 
 public class FluorescenceEffect extends MobEffect {
     public FluorescenceEffect(int color) {
         super(MobEffectCategory.BENEFICIAL, color);
+    }
+
+    @Override
+    public String getDescriptionId() {
+        return "effect.abloom.fluorescence";
+    }
+
+    public String getDescriptionId(LivingEntity entity) {
+        if (entity != null) {
+            ElementType type = AbloomModAttachments.getFluorescenceType(entity);
+            if (type != null) {
+                return "effect.abloom.fluorescence." + type.name().toLowerCase();
+            }
+        }
+        return getDescriptionId();
     }
 
     @Override
